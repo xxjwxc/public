cb5d484ec08842aa51d97a42108191abfe8d7581
xxjwxc <xie1xiao1jun@qq.com>
2020-05-22 01:55:27 +0800

    Update myfile.go

--- myfile/myfile.go
+++ myfile/myfile.go
@@ -3,14 +3,15 @@ package myfile
 import (
 	"fmt"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/xxjwxc/public/tools"
 )
 
 // GetExp 获取字符串后缀
-func GetExp(exp string) string {
-	return path.Ext(exp) //获取文件后缀
+func GetExp(ext string) string {
+	return strings.TrimLeft(path.Ext(ext), ".")
 }
 
 func getFileName(exp string) string {
