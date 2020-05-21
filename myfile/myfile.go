f1012a049cefda91573aaed1ecd7a88acaa02e55
xxjwxc <xie1xiao1jun@qq.com>
2020-05-22 01:41:27 +0800

    file upload

--- myfile/myfile.go
+++ myfile/myfile.go
@@ -2,87 +2,17 @@ package myfile
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"path"
 	"time"
 
 	"github.com/xxjwxc/public/tools"
 )
 
-//上传单个文件
-func UploadOneFile(r *http.Request, field, file_type, dir string) (result bool, file_name string) {
-	//接受post请求
-	if r.Method == "POST" {
-		r.ParseMultipartForm(32 << 20)
-		file_name = getFileName(file_type)
-		//开始存储文件
-		{
-			file, _, err := r.FormFile(field) //文件name
-			defer file.Close()
-			if err != nil {
-				result = false
-			}
-
-			if !tools.CheckFileIsExist(tools.GetModelPath() + "/file/" + dir + "/") {
-				err1 := os.Mkdir(tools.GetModelPath()+"/file/"+dir+"/", os.ModePerm) //创建文件夹
-				if err1 != nil {
-					result = false
-				}
-			}
-
-			f, err := os.OpenFile(tools.GetModelPath()+"/file/"+dir+"/"+file_name, os.O_WRONLY|os.O_CREATE, 0666)
-			defer f.Close()
-			if err != nil {
-				result = false
-			}
-			io.Copy(f, file)
-			result = true
-		}
-
-	} else {
-		result = false
-	}
-	return
+// GetExp 获取字符串后缀
+func GetExp(exp string) string {
+	return path.Ext(exp) //获取文件后缀
 }
 
-//多文件上传
-func UploadMoreFile(r *http.Request, field, file_type, dir string) (result bool, optionDirs []string) {
-	//接受post请求
-	if r.Method == "POST" {
-		r.ParseMultipartForm(32 << 20)
-		files := r.MultipartForm.File[field]
-		l := len(files)
-		//		optionDirs := make([]string, l)
-		for i := 0; i < l; i++ {
-			file, err := files[i].Open()
-			defer file.Close()
-			if err != nil {
-				result = false
-			}
-			file_name := getFileName(file_type)
-
-			if !tools.CheckFileIsExist(tools.GetModelPath() + "/file/" + dir + "/") {
-				err1 := os.Mkdir(tools.GetModelPath()+"/file/"+dir+"/", os.ModePerm) //创建文件夹
-				if err1 != nil {
-					result = false
-				}
-			}
-			f, err := os.OpenFile(tools.GetModelPath()+"/file/"+dir+"/"+file_name, os.O_WRONLY|os.O_CREATE, 0666)
-			defer f.Close()
-			if err != nil {
-				result = false
-			}
-			io.Copy(f, file)
-			optionDirs = append(optionDirs, file_name)
-			result = true
-		}
-
-	} else {
-		result = false
-	}
-	return
-}
 func getFileName(exp string) string {
 	return fmt.Sprintf("%d%s.%s", tools.GetUtcTime(time.Now()), tools.GetRandomString(4), exp)
 }
---
+++ myfile/webFile.go
@@ -0,0 +1,104 @@
+package myfile
+
+import (
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/xxjwxc/public/tools"
+)
+
+type myFile struct {
+	path       string
+	isRelative bool
+}
+
+// NewWebFile 新建文件
+// @parm  path:目录 isRelative:是否相对路径
+func NewWebFile(path string, isRelative bool) *myFile {
+	if !strings.HasSuffix(path, "/") {
+		path += "/"
+	}
+
+	return &myFile{path: path, isRelative: isRelative}
+}
+
+// UploadOneFile 单文件上传
+func (o *myFile) UploadOneFile(r *http.Request, field string) (string, error) {
+	//接受post请求
+	if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		if r.MultipartForm == nil {
+			return "", fmt.Errorf("empty")
+		}
+		_, fh, err := r.FormFile(field) //文件name
+		if err != nil {
+			return "", err
+		}
+		return o.SaveOne(fh)
+	}
+
+	return "", fmt.Errorf("method not support")
+}
+
+// UploadMoreFile 多文件上传
+func (o *myFile) UploadMoreFile(r *http.Request) ([]string, error) {
+	//接受post请求
+	if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		if r.MultipartForm == nil {
+			return []string{}, fmt.Errorf("empty")
+		}
+
+		var files []*multipart.FileHeader
+		for _, v := range r.MultipartForm.File {
+			files = append(files, v...)
+		}
+
+		result := make([]string, 0, len(files))
+		for _, file := range files {
+			src, err := o.SaveOne(file)
+			if err != nil {
+				return []string{}, err
+			}
+			result = append(result, src)
+		}
+
+		return result, nil
+	}
+
+	return []string{}, fmt.Errorf("method not support")
+}
+
+// SaveOne 保存一个
+func (o *myFile) SaveOne(file *multipart.FileHeader) (string, error) {
+	filename := getFileName(GetExp(file.Filename))
+	path := o.path
+	if o.isRelative {
+		path = tools.GetCurrentDirectory() + "/" + path
+	}
+
+	if !tools.CheckFileIsExist(path) {
+		if err := tools.BuildDir(path); err != nil { //创建文件夹
+			return "", err
+		}
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		return "", err
+	}
+	defer src.Close()
+
+	out, err := os.Create(path + filename)
+	if err != nil {
+		return "", err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, src)
+	return o.path + filename, err
+}
