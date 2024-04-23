8fa672d6fcc2a3d4a3cb29f975960e2011caa688
RichieCheng <2229156740@qq.com>
2024-04-23 14:25:48 +0800

    feat: 添加 InValidDel 错误码 (2044-2046)。

--- message/def.go
+++ message/def.go
@@ -155,4 +155,7 @@ const ( //消息id定义
 	AICountIsZero               ErrCode = 2041 // 无AI扩写数据
 	NoFAQQuestion               ErrCode = 2042 // 请先录入FAQ
 	WordLimitError              ErrCode = 2043 // 字数超限
+	InValidDelByTemp            ErrCode = 2044 // 该母版正在被使用，无法删除。
+	InValidDelByVersion         ErrCode = 2045 // 该版本正在被使用，无法删除。
+	InValidDelByScene           ErrCode = 2046 // 该场景正在被使用，无法删除。
 )
